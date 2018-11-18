a01da7186d71ec1ce87ceb22f847a5b6e768194f
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2018-11-18 13:39:46 +0200

    Viikko 3 tehtävä 3

--- Kurssistatistiikka/src/main/java/ohtu/CourseSubmissions.java
+++ Kurssistatistiikka/src/main/java/ohtu/CourseSubmissions.java
@@ -13,17 +13,37 @@ public class CourseSubmissions {
     private int totalExcercisePointsGained;
     private int totalWorkingHours;
     
+    // Stats for all
+    private boolean statsExist;
+    private int students;
+    private double hourTotal;
+    private int exerciseTotal;
+    
     public CourseSubmissions(Course course) {
         this.course = course;
         this.submissions = new ArrayList<>();
         this.totalExcercisePointsGained = 0;
         this.totalWorkingHours = 0;
+        
+        this.statsExist = false;
+        this.students = 0;
+        this.hourTotal = 0.0;
+        this.exerciseTotal = 0;
     }
 
-    public void addSubmission(Submission submission) {
-        this.submissions.add(submission);
-        this.totalExcercisePointsGained += submission.getExercises().length;
-        this.totalWorkingHours += submission.getHours();
+    public void addStudents(int students) {
+        this.students += students;
+        this.statsExist = true;
+    }
+
+    public void addHourTotal(double hourTotal) {
+        this.hourTotal += hourTotal;
+        this.statsExist = true;
+    }
+
+    public void addExerciseTotal(int exerciseTotal) {
+        this.exerciseTotal += exerciseTotal;
+        this.statsExist = true;
     }
     
     public Course getCourse() {
@@ -40,6 +60,28 @@ public class CourseSubmissions {
 
     public int getTotalWorkingHours() {
         return totalWorkingHours;
+    }
+
+    public int getStudents() {
+        return students;
+    }
+
+    public double getHourTotal() {
+        return hourTotal;
+    }
+
+    public int getExerciseTotal() {
+        return exerciseTotal;
+    }
+    
+    public boolean getStatsExist() {
+        return statsExist;
+    }
+
+    public void addSubmission(Submission submission) {
+        this.submissions.add(submission);
+        this.totalExcercisePointsGained += submission.getExercises().length;
+        this.totalWorkingHours += submission.getHours();
     }
     
 }
--- Kurssistatistiikka/src/main/java/ohtu/Main.java
+++ Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -1,6 +1,9 @@
 package ohtu;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
 import java.io.IOException;
 import org.apache.http.client.fluent.Request;
 
@@ -17,28 +20,58 @@ public class Main {
             studentNr = args[0];
         }
 
+        
+        // Getting Courses and submissions
         String url = "https://studies.cs.helsinki.fi/courses/students/"+studentNr+"/submissions";
-
         String bodyText = Request.Get(url).execute().returnContent().asString();
-        
         String urlCourses = "https://studies.cs.helsinki.fi/courses/courseinfo";
-
         String bodyTextCourses = Request.Get(urlCourses).execute().returnContent().asString();
-
         
+        // Parsing Courses and submissions
         Gson mapper = new Gson();
         Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
         Course[] courses = mapper.fromJson(bodyTextCourses, Course[].class);
         
         
         HashMap<String, CourseSubmissions> allCourseSubmissions = new HashMap<>();
-        
         for(Course course : courses) {
             allCourseSubmissions.put(course.getName(), new CourseSubmissions(course));
         }
-        
         for(Submission submission : subs) {
             allCourseSubmissions.get(submission.getCourse()).addSubmission(submission);
+        }
+        
+        
+        // Getting Stats
+        String urlStatsOhtu2018 = "https://studies.cs.helsinki.fi/courses/ohtu2018/stats";
+        String bodyTextStatsOhtu2018 = Request.Get(urlStatsOhtu2018).execute().returnContent().asString();
+        String urlStatsRails2018 = "https://studies.cs.helsinki.fi/courses/rails2018/stats";
+        String bodyTextStatsRails2018 = Request.Get(urlStatsRails2018).execute().returnContent().asString();
+        
+        // Parsing Stats
+        JsonParser parserOhtu2018 = new JsonParser();
+        JsonObject parsedOhtu2018 = parserOhtu2018.parse(bodyTextStatsOhtu2018).getAsJsonObject();
+        JsonParser parserRails2018 = new JsonParser();
+        JsonObject parsedRails2018 = parserRails2018.parse(bodyTextStatsRails2018).getAsJsonObject();
+        
+        
+        for(Map.Entry<String, JsonElement> entry : parsedOhtu2018.entrySet()) {
+            JsonElement jElement = entry.getValue();
+            JsonObject jObject = jElement.getAsJsonObject();
+            
+            CourseSubmissions courseSubmissions = allCourseSubmissions.get("ohtu2018");
+            courseSubmissions.addStudents(jObject.get("students").getAsInt());
+            courseSubmissions.addHourTotal(jObject.get("hour_total").getAsDouble());
+            courseSubmissions.addExerciseTotal(jObject.get("exercise_total").getAsInt());
+        }
+        for(Map.Entry<String, JsonElement> entry : parsedRails2018.entrySet()) {
+            JsonElement jElement = entry.getValue();
+            JsonObject jObject = jElement.getAsJsonObject();
+            
+            CourseSubmissions courseSubmissions = allCourseSubmissions.get("rails2018");
+            courseSubmissions.addStudents(jObject.get("students").getAsInt());
+            courseSubmissions.addHourTotal(jObject.get("hour_total").getAsDouble());
+            courseSubmissions.addExerciseTotal(jObject.get("exercise_total").getAsInt());
         }
         
         
@@ -61,6 +94,14 @@ public class Main {
                 System.out.println("");
                 System.out.println("yhteens\u00e4: " + courseSubmissions.getTotalExcercisePointsGained() + 
                         "/" + courseSubmissions.getCourse().calculateTotalExercisePoints() + " teht\u00e4v\u00e4\u00e4 " + courseSubmissions.getTotalWorkingHours() + " tuntia");
+                
+                // Print stats if they exist.
+                if(courseSubmissions.getStatsExist()) {
+                    System.out.println("");
+                    System.out.println("kurssilla yhteens\u00e4 " + courseSubmissions.getStudents() + 
+                            " palautusta, palautettuja teht\u00e4vi\u00e4 " + courseSubmissions.getExerciseTotal() + 
+                            " kpl, aikaa k\u00e4ytetty yhteens\u00e4 " + (int)courseSubmissions.getHourTotal() + " tuntia");
+                }
             }
         }
         
